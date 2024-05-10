976f19f34865a7b4cbfee643defc8727fa9ee1bd
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2024-05-10 23:49:52 +0200

    Resetting Forms

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -7,6 +7,8 @@ export default function Signup() {
     const data = Object.fromEntries(fd.entries());
     data.acquisition = acquisitionCannel;
     console.log(data);
+
+    e.target.reset();
   }
   return (
     <form onSubmit={handleSubmitForm}>
